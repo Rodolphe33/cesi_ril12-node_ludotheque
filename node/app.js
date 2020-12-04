3cb3c68b676373bbd126377bbad7efc7c490609f
Karmagamemnon <kevinpeeters.perso@gmail.com>
2020-12-04 16:47:19 +0100

    WIP - Adding authentification

--- node/app.js
+++ node/app.js
@@ -1,14 +1,69 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+
+//Configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Initiate our app
+const app = express();
+
+//Configure our app
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'nevergonnagiveyouup', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if (!isProduction) {
+    app.use(errorHandler());
+}
+
+//Configure Mongoose
+mongoose.connect(`mongodb://${hostname}:27017/ludotheque`);
+mongoose.set('debug', true);
+
+//Models & routes
+require('./models/Users');
+require('./config/passport');
+app.use(require('./routes'));
+
+//Error handlers & middlewares
+if (!isProduction) {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.json({
+            errors: {
+                error: err,
+                message: err.message
+            },
+        });
+    });
+}
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        errors: {
+            error: {},
+            message: err.message
+        },
+    });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
