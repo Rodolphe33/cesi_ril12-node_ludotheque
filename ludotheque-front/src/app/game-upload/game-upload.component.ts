9a7a21194a6afe02d69ceb05c3bd4cce3eef5123
Remi Delouche <62890965+Delouche-Remi@users.noreply.github.com>
2021-02-05 16:38:10 +0100

    uploadGame v1

--- ludotheque-front/src/app/game-upload/game-upload.component.ts
+++ ludotheque-front/src/app/game-upload/game-upload.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-upload.component.css']
 })
 export class GameUploadComponent implements OnInit {
+  public gameName;
+  public gameURL;
+  public gameDesc;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addGame() {
+    console.log(this.gameName);
+    console.log(this.gameURL);
+    console.log(this.gameDesc.value);
+ }
 
 }
